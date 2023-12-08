ca7c27f329a6a469ae440f754c4171720196b9b0
Ratul <rajaulislamratul12@gmail.com>
2023-12-08 17:57:59 +0600

    ✨updated accessibility

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
 "use strict";
 
 const imagesEl = document.querySelectorAll(".img");
-const iconsEl = document.querySelectorAll(".icon");
+const buttonsEl = document.querySelectorAll("button");
 const feedbacksEl = document.querySelectorAll(".feedback-container");
-iconsEl.forEach((el) =>
+buttonsEl.forEach((el) =>
   el.addEventListener("click", function (e) {
+    console.log("I was clicked");
     imagesEl.forEach((el) => {
       el.classList.toggle("active");
       el.classList.toggle("inactive");
     });
     feedbacksEl.forEach((el) => {
       el.classList.toggle("visible-review");
+      el.classList.toggle("invisible");
+      el.classList.toggle("visible");
       el.classList.toggle("hidden-review");
     });
   })
